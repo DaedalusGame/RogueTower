86927cd0fbe542995dcd69f2a8c6cf86ed136880
Jonathan Church <jchurch093@gmail.com>
2019-11-30 13:00:35 -0500

    wip weapon class

---
+++ RogueTower/Weapon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace RogueTower
+{
+    abstract class Weapon
+    {
+        public double Damage;
+        public float WeaponSizeMult = 0;
+        public Vector2 WeaponSize;
+
+        public Weapon(Double damage, float weaponSizeMult, Vector2 weaponSize)
+        {
+            Damage = damage;
+            WeaponSizeMult = weaponSizeMult;
+            WeaponSize = weaponSize;
+        }
+
+    }
+
+    class Sword : Weapon
+    {
+        public Sword(Double damage, float weaponSizeMult, Vector2 weaponSize) : base(damage, weaponSizeMult, weaponSize)
+        {
+        }
+    }
+
+}
