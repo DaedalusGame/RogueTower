beffeee72b43fbcf71be0b77a9a11d7f674a9fda
BordListian <bordlistian@hotmail.de>
2019-12-20 00:31:22 +0100

    - Makes the game unclosable

--- RogueTower/Game.cs
+++ RogueTower/Game.cs
@@ -188,9 +188,6 @@ namespace RogueTower
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
             SpriteLoader.Instance.Update(gameTime);
             Scheduler.Instance.Update();
             AudioMgr.Update();
